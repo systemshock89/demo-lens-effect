7c934bf28ad9aa3b43f6029de8845f630bfa1905
systemshock89 <666qqq77>
2023-08-23 11:08:41 +0300

    first

---
+++ readme.md
@@ -0,0 +1,16 @@
+<h1>Parallax 3D lens effect</h1>
+<p><a href="https://systemshock89.github.io/demo-lens-effect/">See demo</a></p>
+
+<h2>Used features</h2>
+<ul>
+    <li>сss transform, transition</li>
+    <li>particles in canvas</li>
+    <li>webpack + gulp</li>
+</ul>
+
+<h2>How to use</h2>
+<ol>
+    <li>git clone</li>
+    <li>npm i</li>
+    <li>gulp</li>
+</ol>
---
+++ src/js/bundle.js
@@ -0,0 +1,22 @@
+import '/src/js/libs/rain.js' // rain canvas
+
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    document.addEventListener('mousemove', e => {
+
+        // добавим к тегу html css переменные
+        // с позицией курсора, причем когда курсор расположен по центру -
+        // картинка должна быть ровной (для этого вычитаем window.innerWidth / 2)
+        // также замедляем вращение - умножаем на .01
+        // чтобы при перемещении в нижний левый угол верхний правый угол не увеличивался, а уменьшался, умножим на -.01 (инвертируем наклон)
+        Object.assign(document.documentElement, {
+            style: `
+            --move-x: ${(e.clientX - window.innerWidth / 2) * -.005}deg; 
+            --move-y: ${(e.clientY - window.innerHeight / 2) * -.01}deg;          
+            `
+        })
+    });
+
+});
